2bee4a76c532386cd44f0eff864aa038d7e98348
Amir <amix@doist.com>
2021-12-10 09:56:55 +0100

    Day 9

--- src/main/advent/9/9_2.kts
+++ src/main/advent/9/9_2.kts
@@ -6,28 +6,22 @@ data class Position(val row: Int, val column: Int)
 val heights = File("data.txt").readLines().map { it.split("").filter { it.length > 0 }.map { it.toInt() } }
 val basinCache = mutableMapOf<Position, Set<Position>>()
 
-fun getLowerValues(currentPosition: Position, directionFn: (position: Position) -> Position): Set<Position> {
-    var curValue = heights[currentPosition.row][currentPosition.column]
-    val position = directionFn(currentPosition)
-    var value = heights.getOrNull(position.row)?.getOrNull(position.column)
-    if (value != null && value != 9 && curValue < value) {
-        return getBasin(position)
-    }
-    else {
-        return emptySet<Position>()
-    }
-}
-
 fun getBasin(position: Position): Set<Position> {
     basinCache[position]?.let { return it }
     val basin = mutableSetOf<Position>()
     val currentHeight = heights[position.row][position.column]
-    if (currentHeight != 9) {
-        basin.add(position)
-        basin.addAll(getLowerValues(position, { p: Position -> Position(p.row - 1, p.column) }))
-        basin.addAll(getLowerValues(position, { p: Position -> Position(p.row, p.column + 1) }))
-        basin.addAll(getLowerValues(position, { p: Position -> Position(p.row + 1, p.column) }))
-        basin.addAll(getLowerValues(position, { p: Position -> Position(p.row, p.column - 1) }))
+    basin.add(position)
+    val adjPositions = sequenceOf(
+        Position(position.row - 1, position.column),
+        Position(position.row, position.column + 1),
+        Position(position.row + 1, position.column),
+        Position(position.row, position.column - 1)
+    )
+    for(adjPosition in adjPositions) {
+        var adjHeight = heights.getOrNull(adjPosition.row)?.getOrNull(adjPosition.column)
+        if (adjHeight != null && adjHeight != 9 && currentHeight < adjHeight) {
+            basin.addAll(getBasin(adjPosition))
+        }
     }
     return basin.also {
         basinCache[position] = it
@@ -44,6 +38,6 @@ if (heights.size > 0) {
     }
 
     basins.sortBy { -it.size }
-    println(basins.subList(0, 3).map { it.size }.reduce { acc, i ->  acc * i})
+    println(basins.take(3).map { it.size }.reduce(Int::times))
 }
 
